cdc1ba53aebcbc1cf1d33c66177bc3ffee9be05b
gustavojb1 <gustavojunqueirab@gmail.com>
2022-10-03 16:25:14 -0300

    criação do script2.js para ao adicionar uma fruta adicionar junto no li

---
+++ script2.js
@@ -0,0 +1,23 @@
+const frutas = ["laranja", "limão", "uva"];
+
+for (i in frutas) {
+  let item = document.getElementById(`fruta-${Number(i) + 1}`);
+  item.innerHTML = frutas[i];
+}
+
+let id = 4;
+
+function addFruta() {
+  let fruta = document.getElementById("input-fruta").value;
+  if (id == 4) {
+    document.getElementById("fruta-4").innerHTML = fruta;
+    id++;
+  } else {
+    frutas.push(fruta);
+    ul = document.getElementById("lista-de-frutas");
+    ul.innerHTML += `<li id="fruta-${id}">${
+      frutas[Number(frutas.length) - 1]
+    }</li>`;
+    id++;
+  }
+}
