fbecfae48b3e682e459f47081afd3642b76b0549
gustavojb1 <gustavojunqueirab@gmail.com>
2022-10-03 14:45:40 -0300

    finished

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let lista = document.querySelectorAll("li");
+for (i in frutas) {
+  lista[i].innerHTML = frutas[i];
+}
+
+document.getElementById("input-fruta").value = "maçã";
+
+function addFruta() {
+  let fruta = document.getElementById("input-fruta").value;
+
+  frutas.push(fruta);
+
+  document.getElementById("fruta-4").innerHTML=frutas[3]
+
+}
